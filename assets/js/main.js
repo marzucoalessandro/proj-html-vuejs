134c55920117a80c1268233f7a665835ce895f7d
Alessandro <marzucoalessandro1@gmail.com>
2021-01-20 19:13:00 +0100

    navbar generata con vue

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+let app = new Vue({
+  el: "#root",
+  data: {
+    logo: [
+      "./assets/img/dark-logo.png"
+    ],
+    myLogo: {
+      width: "250px",
+      marginTop: "30px"
+    },
+    link:[
+      "Home",
+      "Pages",
+      "Courses",
+      "Features",
+      "Blog",
+      "Shop"
+    ],
+    
+
+  }
+
+})
