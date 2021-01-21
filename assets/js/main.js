5ec0498fccb6a74123a99c24af03e19cd9829df0
Alessandro <marzucoalessandro1@gmail.com>
2021-01-21 11:54:25 +0100

    prima parte del footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ let app = new Vue({
       width: "250px",
       marginTop: "30px"
     },
-    link:[
+    link: [
       "Home",
       "Pages",
       "Courses",
@@ -16,8 +16,29 @@ let app = new Vue({
       "Blog",
       "Shop"
     ],
-    
+    address: [
+      "382 NE 191st St # 87394 Miami, FL 33179-3899",
+      "+1(305)547-9909 (9am - 5pm EST, Monday-Friday)",
+      "support@maxcoach.com"
+    ],
+    explore: [
+      "Start Here",
+      "Blog",
+      "About us"
+    ],
+    story: [
+      "Success story",
+      "Courses",
+      "Contact us"
+    ],
+    information: [
+      "Membership",
+      "Purchase guide",
+      "Privacy policy",
+      "Terms of service"
+    ]
 
   }
+
 
 })
