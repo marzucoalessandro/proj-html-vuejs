31c2c96902d18af14734b58bb76ec2c473e9943d
Alessandro <marzucoalessandro1@gmail.com>
2021-01-21 15:55:12 +0100

    generazione card section 5 tramite vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,50 @@ let app = new Vue({
       "Purchase guide",
       "Privacy policy",
       "Terms of service"
+    ],
+    card: [
+      {
+        img: "./assets/img/motivation-course-06-480x298.jpg",
+        price: "$30.00",
+        description: "How to be Successfull: Create A Growth Mindset For Success",
+        lessons: "3 Lessons",
+        students: "50 Students"
+      },
+      {
+        img: "./assets/img/motivation-course-05-480x298.jpg",
+        price: "$30.00",
+        description: "Hot to Build Confidence in Your Abilities",
+        lessons: "1 Lessons",
+        students: "50 Students"
+      },
+      {
+        img: "./assets/img/motivation-course-04-480x298.jpg",
+        price: "$20.00",
+        description: "Productivity Machine - Focus in a Distracted World",
+        lessons: "5 Lessons",
+        students: "50 Students"
+      },
+      {
+        img: "./assets/img/motivation-course-03-480x298.jpg",
+        price: "$20.00",
+        description: "Effective Time Management Mastery - Complete Guide",
+        lessons: "18 Lessons",
+        students: "50 Students"
+      },
+      {
+        img: "./assets/img/motivation-course-02-480x298.jpg",
+        price: "$25.99",
+        description: "Body Language Secrets for Entrepreneurs",
+        lessons: "19 Lessons",
+        students: "50 Students"
+      },
+      {
+        img: "./assets/img/motivation-course-01-480x298.jpg",
+        price: "$19.99",
+        description: "Management Skills: The Science of Leadership",
+        lessons: "17 Lessons",
+        students: "50 Students"
+      }
     ]
-
   }
-
-
-})
+});
